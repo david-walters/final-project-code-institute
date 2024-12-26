79f17a2b0076a02b84c6e2b7dcb28ddef023d1ad
david-walters <dawudwalters@gmail.com>
2024-12-26 02:55:56 +0000

    Change url to use custom view

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import login_view
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('perfume/<int:pk>/', views.perfume_detail, name='perfume_detail'),
     path('cart/', views.cart, name='cart'),
