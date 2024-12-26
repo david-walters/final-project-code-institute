2b5bcadfb832af9488f25e6aa6d42ac06baf7b27
david-walters <dawudwalters@gmail.com>
2024-12-26 02:46:14 +0000

    Lengthen the time of the display of messages

--- shop/static/js/scripts.js
+++ shop/static/js/scripts.js
@@ -6,6 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
             alert.style.transition = 'opacity 0.5s';
             alert.style.opacity = '0';
             setTimeout(() => alert.remove(), 500);
-        }, 5000);
+        }, 10000);
     });
 });
